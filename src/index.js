c949c3cf6c84464ca60ae308236838c0cbbd0a16
Vicko657 <victoriaolusegun@gmail.com>
2024-06-02 15:29:40 +0100

    Forecast HTML is Added via javascript

--- src/index.js
+++ src/index.js
@@ -60,3 +60,24 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("London");
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      ` <div class="col-2 weather-forecast-bg">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">☀️</div>
+            <div class="weather-forecast-temperature">
+              <span class="weather-forecast-temperature-max"> 18°</span>
+              <span class="weather-forecast-temperature-min"> 12°</span>
+            </div>
+          </div>`;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
+displayForecast();
